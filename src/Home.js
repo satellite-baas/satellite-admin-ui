e32e62d4fb43cb4f638aab91c695899dea4426dc
jordanmoore753 <jordanmoore753@gmail.com>
2020-12-02 13:16:52 -0500

    Add health checks for satellite
    Co-authored-by: Jordan Moore <jordanmoore753@gmail.com>
    Co-authored-by: Lewis Reynolds <lr177245@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,61 @@ class Home extends React.Component {
     super(props);
 
     this.state = {
-      healthy: false
+      healthy: false,
+      uptime: 0,
+      intervalID: null
     };
   }
 
   componentDidMount() {
-    // start interval
+    this.healthCheck();
+
+    const intervalID = setInterval(() => {
+      this.healthCheck();
+    }, 5000);
+
+    this.setState({ intervalID });
   }
 
   componentWillUnmount() {
-    // erase interval
+    clearInterval(this.state.intervalID);
   }
+
+  healthCheck = () => {
+    const context = this;
+    const query = `
+      {
+        health {
+          instance
+          status
+          uptime
+        }
+      }
+    `;
+
+    fetch('http://localhost:8080/admin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    })
+    .then(res => res.json())
+    .then(json => {
+      const alpha = json.data.health[json.data.health.length - 1];
+
+      context.setState({
+        healthy: alpha.status === 'healthy',
+        uptime: alpha.uptime
+      });
+    })
+    .catch(err => {
+      context.setState({
+        healthy: false,
+        uptime: 0
+      });
+    })
+  };
 
   render() { 
     return (
